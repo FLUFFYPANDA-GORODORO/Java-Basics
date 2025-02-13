493b9e62a707ab2c9027786d04ba60fd573c7975
~Gaurav Patil <122665090+FLUFFYPANDA-GORODORO@users.noreply.github.com>
2025-02-13 12:10:37 +0530

    Arithmetic operators

---
+++ Program5.java
@@ -0,0 +1,24 @@
+public class Arithmetic {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 5;
+
+        System.out.println("a = " + a + " , b = " + b);
+        System.out.println("Addition : " + (a+b));
+        System.out.println("Subtraction : " + (a-b));
+        System.out.println("Multiplication : " + (a*b));
+        System.out.println("Division : " + (a/b));
+        // Modulus -> Remainder
+        System.out.println("Modulus : " + (a%b));
+        System.out.println();
+        float x = 5;
+        float y = 2;
+
+        System.out.println("x = " + x + " , y = " + y);
+        System.out.println("Addition : " + (x+y));
+        System.out.println("Subtraction : " + (x-y));
+        System.out.println("Multiplication : " + (x*y));
+        System.out.println("Division : " + (x/y));
+
+    }
+}
