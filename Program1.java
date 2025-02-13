eaba8c59ddb28386a01a68778386ea25ff9555eb
~Gaurav Patil <122665090+FLUFFYPANDA-GORODORO@users.noreply.github.com>
2025-02-13 11:06:33 +0530

    Program1

---
+++ Program1.java
@@ -0,0 +1,34 @@
+// Variables in java 
+
+public class Variables {
+    public static void main(String[] args){
+
+        // Integer -> -2147483648 to 2147483647
+        // Short -> -32768 to 32767 
+        // Long -> -9223372036854775808 to 9223372036854775807
+        int a = 45;
+        System.out.println(a);
+        // Value can be changed 
+        a = 30;
+        System.out.println(a);
+
+        // Float  -> 1.40239846e-45f to 3.40282347e+38f
+        float b = 5;
+        System.out.println(b);
+
+        // Double -> 4.94065645841246544e-324 to 1.79769313486231570e+308  
+        double c = 3.14;
+        System.out.println(c);
+
+        // Boolean -> true/false
+        boolean d = true;
+        System.out.println(d);
+
+        // String -> Collection of characters
+        String str = "Hello World";
+        System.out.println(str);
+    }
+}
+
+// \n -> It is used to go to the next line
+// \t -> It is used to give 4 spaces gap between any two words
